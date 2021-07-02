963ba71bb1f3b6946f52c9e06aedef3cb442fd30
Paddy Mullen <paddy@paddymullen.com>
2021-07-02 12:00:42 -0400

    WIP

--- personal/keys.el
+++ personal/keys.el
@@ -40,6 +40,8 @@
 
 (global-unset-key (kbd "M-`") )
 
+
+;; want to move all of these window bindings to "M-s-???"
 (global-set-key (kbd "s-P") 'enlarge-window)
 (global-set-key (kbd "s-p") 'windmove-up)
 (global-set-key (kbd "s-N") 'shrink-window)
@@ -47,7 +49,7 @@
 (global-set-key (kbd "s-F") '_paddy-enlarge-window-horizontal)
 (global-set-key (kbd "s-f") 'windmove-right)
 (global-set-key (kbd "s-B") '_paddy-shrink-window-horizontal)
-(global-set-key (kbd "s-b") 'windmove-left)
+
 (global-set-key (kbd "s-w") 'delete-window)
 (global-set-key (kbd "s-o") 'other-window)
 
@@ -56,12 +58,37 @@
 (global-set-key (kbd "s-g") 'magit-status)
 (global-set-key (kbd "s-w") 'delete-window)
 (global-set-key (kbd "s-=") 'balance-windows)
+(global-set-key (kbd "s-b") 'ivy-switch-buffer)
+
+
+; want a function that rotates the case of word, not important, rarely use this
+(global-set-key (kbd "M-L") 'downcase-word)
 
 (global-set-key (kbd "C-S-k") 'paddy-kill-from-begining-of-line)
 (global-set-key (kbd "C-s-f") 'paddy-put-buffer-filename-in-killring)
 (global-set-key (kbd "s-C-x C-k") 'copy-region-as-kill)
 (global-set-key (kbd "s-M-f") 'find-file-at-point)
-(global-set-key (kbd "C-x C-S-f") 'find-file-other-window)
+(global-set-key (kbd "C-x C-M-f") 'find-file-other-window)
+
+(global-set-key (kbd "M-[")  'xref-find-definitions)
+(global-set-key (kbd "M-]")  'xref-find-references)
+
+
+;; (defadvice kill-region (before slick-cut activate compile)
+;;   "When called interactively with no active region, kill a single line instead."
+;;   (interactive
+;;    (if mark-active (list (region-beginning) (region-end))
+;;      (list (line-beginning-position)
+;;            (line-beginning-position 2)))))
+
+;; (defadvice kill-region (before slick-cut activate compile)
+;;   "When called interactively with no active region, kill a single line instead."
+;;   (interactive
+;;    (if buffer-read-only
+;;        (call-interactively 'copy-region-as-kill )
+;;      ad-do-it
+;;      )))
+
 
 (provide 'keys)
 ;;; keys.el ends here
--- personal/lsp-setup.el
+++ personal/lsp-setup.el
@@ -34,13 +34,6 @@
 (define-key global-map [remap switch-to-buffer] #'helm-mini)
 
 (which-key-mode)
-;; (add-hook 'c-mode-hook 'lsp)
-;; (add-hook 'c++-mode-hook 'lsp)
-(add-hook 'prog-mode-hook #'lsp)
-;; (add-hook 'python-mode
-;;           (lambda ()
-;;             (require 'lsp-python-ms)
-;;             (lsp)))
 
 
 (use-package dap-mode
@@ -75,9 +68,69 @@
       company-minimum-prefix-length 1
       lsp-idle-delay 0.1)  ;; clangd is fast
 
+
+;; (add-hook 'c-mode-hook 'lsp)
+;; (add-hook 'c++-mode-hook 'lsp)
+;; (add-hook 'typescript-mode 'lsp)
+;; (add-hook 'javascript-mode 'lsp)
+
+                                        ;(add-hook 'prog-mode-hook #'lsp)
+;; (add-hook 'python-mode
+;;           (lambda ()
+;;             (require 'lsp-python-ms)
+;;             (lsp)))
+
+
+;making room for M-l to be lsp basemap
+;(global-unset-key (kbd "M-l"))
+
+
 (use-package lsp-mode
+;  :straight t
+  :commands (lsp lsp-deferred)
   :hook
-  ((python-mode . lsp)) )
+  ((python-mode . lsp))
+  ((typescript-mode js2-mode web-mode) . lsp)
+  :init
+  (setq lsp-keymap-prefix (kbd "M-l"))
+ )
+
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :config
+  (setq typescript-indent-level 2))
+
+;; (defun dw/set-js-indentation ()
+;;   (setq js-indent-level 2)
+;;   (setq evil-shift-width js-indent-level)
+;;   (setq-default tab-width 2))
+
+(use-package js2-mode
+  :mode "\\.jsx?\\'"
+  :config
+  ;; Use js2-mode for Node scripts
+  (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
+
+  ;; Don't use built-in syntax checking
+  (setq js2-mode-show-strict-warnings nil)
+
+  ;; Set up proper indentation in JavaScript and JSON files
+  (add-hook 'js2-mode-hook #'dw/set-js-indentation)
+  (add-hook 'json-mode-hook #'dw/set-js-indentation))
+
+
+;; (use-package apheleia
+;;   :config
+;;   (apheleia-global-mode +1))
+
+(use-package prettier-js
+  ;; :hook ((js2-mode . prettier-js-mode)
+  ;;        (typescript-mode . prettier-js-mode))
+  :config
+  (setq prettier-js-show-errors nil))
+
+
+
 (use-package lsp-ui
    :commands lsp-ui-mode)
 
@@ -95,6 +148,12 @@
   (yas-global-mode))
 
 
+;this seems to have a huge performance gain
+(setq lsp-log-io nil)
+;(setq lsp-log-io 't )
+
 ;setting company-idle-delay to 1.5 makes it much less likel that slow
 ;company-mode completions will delay my typing
-(setq company-idle-delay 1.5)
+;(setq company-idle-delay .75)
+(setq company-idle-delay 0)
+
